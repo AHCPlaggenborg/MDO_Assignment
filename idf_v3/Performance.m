808c6df2a8314a599e172d7c500b5b65fed3b782
marie-lecarpentier <130735307+marie-lecarpentier@users.noreply.github.com>
2025-01-13 14:28:35 +0100

    Update Performance.m

--- idf_v3/Performance.m
+++ idf_v3/Performance.m
@@ -8,9 +8,9 @@ R = 8334000; %m, calculated from design range of 4500 NM
 nu = exp(-(V-239.217)^2/(2*70^2) - (h_cr-10058.4)^2/(2*2500^2));
 CT = 1.8639e-4/nu;
 %CL_CD = CL_w/(CD_w + CD_AW);
-CL_CD = 16;
-%CL_CD = 1/(CD_AW/CLw + 1/CLw_CDw);
+%CL_CD = 16;
+CL_CD = 1/(CD_AW/CLw + 1/CLw_CDw);
 K = 1-0.938*exp(-R*CT/(V*CL_CD));
-Wfuel = K*210000;
+Wfuel = K*I.Weight.MTOW;
 
 end
